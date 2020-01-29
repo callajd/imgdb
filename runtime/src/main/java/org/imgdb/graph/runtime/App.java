16f66abb7761dcf9b6553bb599f8ea827cceb165
Jeff Callahan <jeff.callahan@nordstrom.com>
2020-01-28 20:20:18 -0800

    Create initial project structure

---
+++ runtime/src/main/java/org/imgdb/graph/runtime/App.java
@@ -0,0 +1,8 @@
+package org.imgdb.graph.runtime;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
---
+++ types/src/main/java/org/imgdb/graph/types/App.java
@@ -0,0 +1,8 @@
+package org.imgdb.graph.types;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
